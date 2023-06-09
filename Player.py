d0e1e819419fbf7e795e8bae40f4c3518fdbffc2
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-09 14:15:07 -0400

    header gui

--- Player.py
+++ Player.py
@@ -8,4 +8,9 @@ from tkinter.messagebox import *
 from tkinter import ttk
 import time
 
+root = Tk()
+root.geometry("865x460+250+100")
+root.title("Mp3 Music Player")
+root.configure(bg="black")
+root.resizable(width=0, height=0)
 
