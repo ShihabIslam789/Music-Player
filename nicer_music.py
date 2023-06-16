8950e6e93f4c8a6ef30625f3fb0169fa4aff28e1
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-16 15:05:23 -0400

    dependencies.

---
+++ nicer_music.py
@@ -0,0 +1,11 @@
+from tkinter import (
+    Tk,HORIZONTAL,Label,Button,
+    END,PhotoImage,Listbox,
+    LEFT,StringVar,Menu,Toplevel,
+    ACTIVE,BOTH,TOP
+    )
+from tkinter.filedialog import askdirectory
+from tkinter.messagebox import showerror,askquestion,showinfo
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from ttkbootstrap.icons import Emoji
