281fda84026f22f429a5bd2e01587d2930e2550a
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-06-17 17:46:54 -0400

    library importer.

--- nicer_music.py
+++ nicer_music.py
@@ -9,3 +9,17 @@ from tkinter.messagebox import showerror,askquestion,showinfo
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.icons import Emoji
+
+
+import os
+import threading
+import time
+
+from PIL import Image, ImageTk
+import pygame
+from mutagen.mp3 import MP3
+
+
+CURRENT_SONG_INDEX = 0
+NUMBER_OF_SONGS_IN_LIST = 0
+IMAGE_PATH = 'images/best.png'
